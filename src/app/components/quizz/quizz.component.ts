0ca4fe2b62291880adbbcd1287897741dc67da5d
Giovane Souza <developergiovanesouza@gmail.com>
2023-12-01 19:31:24 -0300

    feat: regras do jogo

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// Importação do quizz JSON
+import quizz_questions from '../../../assets/data/quizz_questions.json';
 
 @Component({
   selector: 'app-quizz',
@@ -6,7 +8,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quizz.component.css'],
 })
 export class QuizzComponent implements OnInit {
-  
   title: string = '';
 
   questions: any;
@@ -22,5 +23,55 @@ export class QuizzComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // Verifica se os dados estão em memória
+    if (quizz_questions) {
+      this.finished = false;
+      this.title = quizz_questions.title;
+
+      this.questions = quizz_questions.questions;
+      this.questionSelected = this.questions[this.questionIndex];
+
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
+
+      console.log(this.questionIndex);
+      console.log(this.questionMaxIndex);
+    }
+  }
+
+  playerChoose(value: string) {
+    this.answers.push(value);
+    console.log(this.answers);
+    this.nextStep();
+  }
+
+
+  async nextStep() {
+    this.questionIndex += 1;
+
+    if (this.questionMaxIndex > this.questionIndex) {
+      this.questionSelected = this.questions[this.questionIndex];
+    } else {
+      const finalAnswer:string = await this.checkResult(this.answers)
+      this.finished = true;
+      this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results ]
+    }
+  }
+
+
+  async checkResult(anwsers: string[]) {
+    const result = anwsers.reduce((previous, current, i, arr) => {
+      if (
+        arr.filter((item) => item === previous).length >
+        arr.filter((item) => item === current).length
+      ) {
+        return previous;
+      } else {
+        return current;
+      }
+    });
+
+    return result;
+  }
 }
