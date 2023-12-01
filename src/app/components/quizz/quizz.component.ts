7cd1ecd1afdf22c1d78062f7e5032a9ca9d2a4ab
Giovane Souza <developergiovanesouza@gmail.com>
2023-12-01 18:56:50 -0300

    feat: propriedades lógicas

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
-  styleUrls: ['./quizz.component.css']
+  styleUrls: ['./quizz.component.css'],
 })
 export class QuizzComponent implements OnInit {
+  
+  title: string = '';
 
-  constructor() { }
+  questions: any;
+  questionSelected: any;
 
-  ngOnInit(): void {
-  }
+  answers: string[] = [];
+  answerSelected: string = '';
 
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
+
+  finished: boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
